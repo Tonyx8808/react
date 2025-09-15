af0a041c684f4e32e05dcb30bb2da7180138b051
Antonio Russo <antoniorusso1988@outlook.it>
2025-09-15 10:04:15 +0200

    third button reset

--- helloword/src/components/contatore.jsx
+++ helloword/src/components/contatore.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
-
+//button increment
   const handleIncrement = () => {
     setCount((_count) => _count + 1);
   };
-
+//button decrement
   const handleDecrement = () => {
     setCount((_count) => _count - 1);
+  };
+  //button reset
+
+  const handleReset = () => {
+    setCount(0);
   };
 
   return (
@@ -16,6 +21,7 @@ function Counter() {
       <p>Count: {count}</p>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
