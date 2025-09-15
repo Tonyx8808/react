dcf6eee94741e676fcd8688cdbfe0819a3367486
Antonio Russo <antoniorusso1988@outlook.it>
2025-09-15 09:58:19 +0200

    button decrement

--- helloword/src/components/contatore.jsx
+++ helloword/src/components/contatore.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function Counter (){
-    const [count, setCount] = useState(0);
-   const handleIncrement = () => {
-    setCount(count+1);
-   };
-   return(
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((_count) => _count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((_count) => _count - 1);
+  };
+
+  return (
     <div>
-        <p>Count: {count}</p>
-        <button onClick={handleIncrement}>Increment</button>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
-   );
+  );
 }
 
-export default Counter
+export default Counter;
