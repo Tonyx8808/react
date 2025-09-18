41e884f7e41ab5424caa6f1c577ac6f3e3ac9eb0
Antonio Russo <antoniorusso1988@outlook.it>
2025-09-18 10:35:03 +0200

    card prop children

--- Lista.jsx
+++ Lista.jsx
@@ -1,37 +1,60 @@
 import React from "react";
-import ItemList from "./ItemList";
-const Lista = () =>{
 
-const onePiece = ["Luffy",
-     "Zoro",
-      "Nami", 
-      "Usop", 
-      "Sanji", 
-      "Chopper", 
-      "Nico Robin", 
-      "Brook", 
-      "Franky",
-       "Jinbee"];
-    
-       const devilFruits =["Gom Gom",
-        "Puzzle Puzzle",
-        "Dark Dark",
-        "Ope Ope",
-        "Yomi Yomi",
-        "Foco Foco",
-        "Filo Filo"
-       ]
-
-return(
-    <>
-    <h1>One Peace</h1>
-    <h2>Ciurma di Cappello di paglia</h2>
-    <ItemList items={onePiece} />
-    <h2>Devil Fruits</h2>
-    <ItemList items={devilFruits} />
-    </>
-);
-
+const Card = ({ children }) => {
+  return (
+    <div style={{ border: "1px solid black", padding: "10px", margin: "10px 0" }}>
+      {children}
+    </div>
+  );
 };
 
-export default Lista
+const Lista = () => {
+  const onePiece = [
+    "Luffy",
+    "Zoro",
+    "Nami",
+    "Usopp",
+    "Sanji",
+    "Chopper",
+    "Nico Robin",
+    "Brook",
+    "Franky",
+    "Jinbe"
+  ];
+
+  const devilFruits = [
+    "Gom Gom",
+    "Puzzle Puzzle",
+    "Dark Dark",
+    "Ope Ope",
+    "Yomi Yomi",
+    "Foco Foco",
+    "Filo Filo"
+  ];
+
+  return (
+    <div>
+      <h1>One Piece</h1>
+
+      <Card>
+        <h2>Ciurma di Cappello di Paglia</h2>
+        <ul>
+          {onePiece.map((personaggio) => (
+            <li key={personaggio}>{personaggio}</li>
+          ))}
+        </ul>
+      </Card>
+
+      <Card>
+        <h2>Devil Fruits</h2>
+        <ul>
+          {devilFruits.map((frutto) => (
+            <li key={frutto}>{frutto}</li>
+          ))}
+        </ul>
+      </Card>
+    </div>
+  );
+};
+
+export default Lista;
